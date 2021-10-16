fb2e21b206764508803c8d34a0e809b70c19c74f
Vasko <magaranov@abv.bg>
2021-10-16 20:39:50 +0300

    abilities are interrupted by movement

--- Assets/Scripts/Gruntz/Abilities/AbilityManagerDef.cs
+++ Assets/Scripts/Gruntz/Abilities/AbilityManagerDef.cs
@@ -6,6 +6,8 @@ namespace Gruntz.Abilities
     public class AbilityManagerDef : Def, IRuntimeInstance
     {
         public MessagesBoxTagDef AbilityMessages;
+        public MessagesBoxTagDef NavigationMessages;
+
         IContextObject IRuntimeInstance.CreateRuntimeInstance()
         {
             return new AbilityManager();
