ec9cb0a476a2fc9984f96dd9fb698845a6cee422
Vasko <magaranov@abv.bg>
2021-11-02 18:13:35 +0200

    menu beautification

--- Assets/Scripts/Gruntz/UI/Menu.cs
+++ Assets/Scripts/Gruntz/UI/Menu.cs
@@ -2,24 +2,50 @@ using Base;
 using System.Linq;
 using UnityEngine;
 
-namespace Utils.Test
+namespace Gruntz.UI
 {
-    public class SaveGame : MonoBehaviour
+    public class Menu : MonoBehaviour
     {
+        public GameObject OpenMenu;
+        public GameObject ClosedMenu;
+
+        public GameObject QuickLoad;
+
         public TagDef QuickSave;
         public TagDef LevelProgressSaveTag;
+
+        public void RefreshLoadButton()
+        {
+            QuickLoad.SetActive(false);
+            var game = Game.Instance;
+            var savesManager = game.SavesManager;
+            var save = savesManager.Saves.LastOrDefault(x => x.SaveTag == QuickSave);
+            if (save != null) {
+                QuickLoad.SetActive(true);
+            }
+        }
+
+        public void Toggle()
+        {
+            OpenMenu.gameObject.SetActive(!OpenMenu.gameObject.activeSelf);
+            ClosedMenu.gameObject.SetActive(!OpenMenu.gameObject.activeSelf);
+            RefreshLoadButton();
+        }
+
         public void Save()
         {
             var game = Game.Instance;
             var savesManager = game.SavesManager;
             savesManager.CreateSave(QuickSave);
+            RefreshLoadButton();
         }
         public void LoadLast()
         {
             var game = Game.Instance;
             var savesManager = game.SavesManager;
             var save = savesManager.Saves.LastOrDefault(x => x.SaveTag == QuickSave);
-            if (save != null) {
+            if (save != null)
+            {
                 savesManager.LoadSave(save, () => { });
             }
         }
@@ -35,7 +61,8 @@ namespace Utils.Test
             var game = Game.Instance;
             var savesManager = game.SavesManager;
             var save = savesManager.Saves.LastOrDefault(x => x.SaveTag == LevelProgressSaveTag);
-            if (save != null) {
+            if (save != null)
+            {
                 savesManager.LoadSave(save, () => { });
             }
         }
