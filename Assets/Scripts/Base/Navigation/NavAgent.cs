8043f6f97454e5f45f8e43d456599cc08e156051
Vasil <magaranov@abv.bg>
2021-12-29 00:50:19 +0200

    push back wip

--- Assets/Scripts/Base/Navigation/NavAgent.cs
+++ Assets/Scripts/Base/Navigation/NavAgent.cs
@@ -8,6 +8,11 @@ namespace Base.Navigation
 {
     public class NavAgent : IActorComponent, IOrderedUpdate, ISerializedObject
     {
+        private class Push
+        {
+            public Vector3 Destination;
+        }
+
         [Serializable]
         public class SimpleNavTarget : INavigationTarget
         {
@@ -121,8 +126,7 @@ namespace Base.Navigation
         {
             var navigation = Navigation.GetNavigationFromContext();
 
-            var request = new MoveRequest
-            {
+            var request = new MoveRequest {
                 Obstacles = _navAgentData.Obstacles,
                 CurrentPos = _navAgentBehaviour.ActorVisuals.position,
                 TargetPos = _navAgentData.Target,
@@ -132,7 +136,71 @@ namespace Base.Navigation
                 MoveResultCallback = Move
             };
 
+            if (CurrentPush != null)
+            {
+                request = new MoveRequest {
+                    Obstacles = _navAgentData.Obstacles,
+                    CurrentPos = _navAgentBehaviour.ActorVisuals.position,
+                    TargetPos = new SimpleNavTarget { Target = CurrentPush.Destination },
+                    MoveSpeed = _navAgentData.Speed,
+                    TravelSegmentInfo = new Base.Navigation.TravelSegmentInfo { StartPos = Pos, EndPos = Pos },
+                    CheckForSegmentInfoClashes = _navAgentData.CheckForSegmentInfoClashes,
+                    MoveResultCallback = HandlePush
+                };
+            }
+
             navigation.MakeMoveRequest(request);
+        }
+
+        private Push CurrentPush = null;
+
+        private void HandlePush(MoveRequestResult moveRequestResult)
+        {
+            if (!_pushPrecessed && Navigation.AreVectorsTheSame(moveRequestResult.PositionToMove, Pos)) {
+                _pushPrecessed = true;
+                var travelSegmentInfo = new TravelSegmentInfo(this);
+                Target = new SimpleNavTarget {Target = travelSegmentInfo.EndPos};
+                CurrentPush = null;
+                return;
+            }
+            _pushPrecessed = true;
+            
+            var messagesSystem = MessagesSystem.MessagesSystem.GetMessagesSystemFromContext();
+            if ((_navAgentBehaviour.ActorVisuals.position - moveRequestResult.PositionToMove).sqrMagnitude > 0.001) {
+                messagesSystem.SendMessage(NavAgentComponentDef.NavigationMessages, MainUpdaterUpdateTime.FixedCrt, Actor, "moving");
+            }
+            else {
+                messagesSystem.SendMessage(NavAgentComponentDef.NavigationMessages, MainUpdaterUpdateTime.FixedCrt, Actor, "staying");
+            }
+
+            _navAgentBehaviour.ActorVisuals.position = moveRequestResult.PositionToMove;
+            _navAgentBehaviour.NavObstacle.transform.position = moveRequestResult.TravelSegmentInfo.EndPos;
+            _navAgentBehaviour.LocalTravelStartPoint.position = moveRequestResult.TravelSegmentInfo.StartPos;
+            
+            var gameplayManager = GameplayManager.GetGameplayManagerFromContext();
+            gameplayManager.HandleGameplayEvent(new ActorTouchedPositionGameplayEvent {
+                Actor = Actor,
+                Positions = moveRequestResult.TouchedPositions
+            });
+            
+            if (Navigation.AreVectorsTheSame(CurrentPush.Destination, moveRequestResult.PositionToMove)) {
+                Target = new SimpleNavTarget {Target = CurrentPush.Destination};
+                CurrentPush = null;
+            }
+        }
+
+        private bool _pushPrecessed = true;
+        public void RandomPush()
+        {
+            var navigation = Navigation.GetNavigationFromContext();
+            var map = navigation.Map;
+            var snapped = map.SnapPosition(Pos);
+            var neighbours = map.GetNeighbours(snapped).ToList();
+            int randomIndex = Game.Instance.Random.Next() % neighbours.Count;
+
+            Vector3 randomNeighbour = neighbours[randomIndex];
+            CurrentPush = new Push {Destination = randomNeighbour};
+            _pushPrecessed = false;
         }
 
         public void Init()
